ff0a195bf89fbec3c003881b54c2bb69a0ea7212
Abhishek Banerji <webdesign.banerji@gmail.com>
2020-04-02 14:44:29 -0400

    Welcome Controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+<<<<<<< HEAD
   namespace :manage do
     get "welcome/index", to: "welcome#index"
   end
@@ -12,4 +13,9 @@ Rails.application.routes.draw do
 
   root "manage/welcome#index"
   
+=======
+  get 'welcome/index'
+
+  root 'welcome#index'
+>>>>>>> Welcome Controller
 end
