467019677c2663dc299aee74264f88b492e8987e
Abhishek Banerji <webdesign.banerji@gmail.com>
2020-04-03 16:52:41 -0400

    Basic CRUD setup for Retailer:
    - Setup namespace "manage" in routing and adapted the app to use the namespace
    - Setup just the model for Retailers. Have not made any accomodations for other associated models.
    - Tweaked locales/en.yml file to change the default name for Retailer.uri_string and the default submit button text on forms.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-<<<<<<< HEAD
   namespace :manage do
     get "welcome/index", to: "welcome#index"
   end
@@ -13,9 +12,4 @@ Rails.application.routes.draw do
 
   root "manage/welcome#index"
   
-=======
-  get 'welcome/index'
-
-  root 'welcome#index'
->>>>>>> Welcome Controller
 end
